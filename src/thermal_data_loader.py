b7cafdd2043b0bc879431c62fc962ebc3c0d2613
hdzefe <131723136+hdzefe@users.noreply.github.com>
2026-09-14 13:03:11 +0200

    Simplify debug CSV function signature - remove depth_col parameter

--- src/thermal_data_loader.py
+++ src/thermal_data_loader.py
@@ -1,21 +1,31 @@
-    def save_debug_borehole_csv(self, horizon_name, merged_valid, geotis_available, temperature_at_boreholes, gradient_temps, temp_blended, depth_col):
+    def save_debug_borehole_csv(self, horizon_name, merged_valid, geotis_available, temperature_at_boreholes, gradient_temps, temp_blended):
         """
         Save debug CSV with all borehole data used in interpolation
         
         Args:
             horizon_name: Horizon name
-            merged_valid: DataFrame with valid boreholes
+            merged_valid: DataFrame with valid boreholes (includes mean_depth already calculated)
             geotis_available: Boolean array (True = GeoTIS data available)
             temperature_at_boreholes: Array of GeoTIS temperatures
             gradient_temps: Array of gradient-calculated temperatures
             temp_blended: Final blended temperatures used for RBF
-            depth_col: Name of depth column (het1_mean_depth, etc.)
         """
+        # Extract mean depth from merged_valid (should have the depth_col already)
+        depth_col_map = {
+            'het1': 'het1_mean_depth',
+            'het2': 'het2_mean_depth',
+            'sin1': 'sin1_mean_depth',
+            'sin2': 'sin2_mean_depth',
+            'pli1': 'pli1_mean_depth',
+            'pli2': 'pli2_mean_depth',
+        }
+        depth_col = depth_col_map.get(horizon_name, 'mean_depth')
+        
         debug_df = pd.DataFrame({
             'borehole_name': merged_valid['borehole_name'].values if 'borehole_name' in merged_valid.columns else range(len(merged_valid)),
             'x': merged_valid['x'].values,
             'y': merged_valid['y'].values,
-            'mean_depth_m': merged_valid[depth_col].values,
+            'mean_depth_m': abs(merged_valid[depth_col].values),  # Convert back to positive for readability
             'geotis_available': geotis_available,
             'geotis_temperature_C': temperature_at_boreholes,
             'gradient_temperature_C': gradient_temps,
